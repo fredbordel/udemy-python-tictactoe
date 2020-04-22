5fc9e010b569ecb92c408781c79f4b92fd56b4d6
Fred <frederique.bordeleau@gmail.com>
2020-04-22 11:52:34 -0400

    display_board implemented

--- game.py
+++ game.py
@@ -5,11 +5,20 @@ from IPython.display import clear_output
 
 
 def display_board(board):
-    for each in board:
-        print(' | |')
-        print(f'| {each} |')
-        print(' | |')
+
+    third_row = ""
+    second_row = ""
+    first_row = ""
+    for each in range(1, 4):
+        third_row += f'{board[each]}|'
+    for each in range(4, 7):
+        second_row += f'{board[each]}|'
+    for each in range(7, 10):
+        first_row += f'{board[each]}|'
+
+    print("-------")
+    print(f'|{first_row} \n|{second_row} \n|{third_row}')
 
 
-test_board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 display_board(test_board)
